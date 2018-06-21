4a0f9d2931c09858e50463bd3303eb9787f66253
Taskkill <johny95wil@gmail.com>
2018-06-21 21:55:10 +0200

    remake

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # snippet_runner_js
 Simple JS code snippet runner
+
+API:
+``` js
+window.snippet_js.evaluate( source : string, traps : object )
+```
+
+## source - string
+it is standard javascript string containing your code
+
+## traps - object
+it should be object with defined properties, these properties, when used as variables in snippet code, will be trapped and your definitions will be used
+for example:
+
+``` js
+snippet_js.evaluate(
+  'console.log("hello world")',
+  {
+    console: {
+      log(content) {
+        // here you can do wathever you like with content
+        alert(content)
+      }
+    }
+  }
+)
+```
+the above code snippet contains execution of console.log, but we redefined console.log for snippet using `traps` object, thus in reality, alert will be called as result of evaluating snippet code
+
+you can use traps to override virtually any variable
+
+if not supplied by you, `traps` is empty object, such evaluation will be completely transparent, without overriding anything
+
+
+## Exceptions
+it can throw - because provided snippet code can be invalid, you better call `snippet_js.evaluate` inside try {} catch () {} clause
+
--- main.js
+++ main.js
@@ -1,170 +1,46 @@
 (function() {
-  // expose control object to global scope
-  let terminal = null;
-
   window.snippet_js = {
-    evaluate(source, traps, allowed) {
-      runInSandbox(source, traps, allowed);
-    },
-    resetTerminal() {
-      terminal = null;
-    },
-    commit(source, traps = {}, allowed = {}) {
-      if (terminal === null) {
-        terminal = runSandboxTerminal(source, traps, allowed);
-        terminal.next(source);
-        return;
-      }
-      terminal.next(source);
-    }
-  };
+    evaluate: ( source, traps ) => run_in_sandbox(source, traps),
+  }
 
-  function runInSandbox(source, traps = {}, allowed = {}) {
-    const err = traps.console.error || console.error;
-
-    // alowing eval for myself
-    allowed['eval'] = null;
-
+  function run_in_sandbox ( source, traps = {} ) {
+    const err = traps.console.error || console.error
     const scope = new Proxy(
-      {
-        source: source
-      },
-      {
-        has(target, propName) {
-          if (propName in traps) {
-            return true;
-          }
-
-          if (propName in allowed) {
-            return false;
-          }
-
-          if (propName in window) {
-            err(
-              "Error - DO NOT USE: '" +
-                propName +
-                "' IT IS RESTRICTED GLOBAL VARIABLE"
-            );
-            throw 'ERROR USE OF PROTECTED GLOBAL VARIABLES';
-          }
-
-          if (propName in target) {
-            return true;
-          }
-
-          // check if var or function variable infected current function's scope
-          let searchedVar;
-          try {
-            eval(`searchedVar = ${propName}`);
-          } catch (Ex) {
-            // searched for var not in global scope, not supplied from upper scope
-            // this var doesn't exist - throw Exception and log error
-            err('ERROR - VARIABLE ' + propName + ' was not declared!');
-            throw 'Undeclared variable use Exception';
-          }
-
-          if (typeof searchedVar === 'function') {
-            return false;
-          }
-
-          return true;
-        },
-        set(target, propName, value) {
-          if (traps[propName]) {
-            traps[propName] = value;
-          } else {
-            target[propName] = value;
-          }
-
-          return true;
-        },
-        get(target, propName) {
-          if (traps[propName]) {
-            return traps[propName];
-          }
-
-          return target[propName];
+    {
+      source,
+    },
+    {
+      has( target, prop_name ) {
+        if (prop_name in traps) {
+          return true
         }
-      }
-    );
+
+        let searched_var
+        try {
+          eval(`searched_var = ${prop_name}`)
+        } catch (Ex) {
+          err(`ReferenceError: ${prop_name} was not declared`)
+          throw `Undeclared variable use Exception`
+        }
+        
+        return ! typeof searched_var === 'function'
+      },
+
+      set( target, prop_name, value ) {
+        if (traps[prop_name])
+          traps[prop_name] = value
+        else
+          target[prop_name] = value
+        
+        return true
+      },
+
+      get: ( target, prop_name ) =>
+        traps[prop_name] ? traps[prop_name] : target[prop_name],
+    })
 
     with (scope) {
-      eval(source);
+      eval(source)
     }
   }
-
-  function* runSandboxTerminal(source, traps = {}, allowed = {}) {
-    const err = traps.console.error || console.error;
-
-    // alowing eval for myself
-    allowed['eval'] = null;
-    const target = {
-      source: source
-    };
-
-    const scope = new Proxy(target, {
-      has(target, propName) {
-        if (propName in traps) {
-          return true;
-        }
-
-        if (propName in allowed) {
-          return false;
-        }
-
-        if (propName in window) {
-          err(
-            "Error - DO NOT USE: '" +
-              propName +
-              "' IT IS RESTRICTED GLOBAL VARIABLE"
-          );
-          throw 'ERROR USE OF PROTECTED GLOBAL VARIABLES';
-        }
-
-        if (propName in target) {
-          return true;
-        }
-
-        // check if var or function variable infected current function's scope
-        let searchedVar;
-        try {
-          eval(`searchedVar = ${propName}`);
-        } catch (Ex) {
-          // searched for var not in global scope, not supplied from upper scope
-          // this var doesn't exist - throw Exception and log error
-          err('ERROR - VARIABLE ' + propName + ' was not declared!');
-          throw 'Undeclared variable use Exception';
-        }
-
-        if (typeof searchedVar === 'function') {
-          return false;
-        }
-
-        return true;
-      },
-      set(target, propName, value) {
-        if (traps[propName]) {
-          traps[propName] = value;
-        } else {
-          target[propName] = value;
-        }
-
-        return true;
-      },
-      get(target, propName) {
-        if (traps[propName]) {
-          return traps[propName];
-        }
-
-        return target[propName];
-      }
-    });
-
-    while (true) {
-      with (scope) {
-        eval(source);
-      }
-      target.source = yield;
-    }
-  }
-})();
+})()
